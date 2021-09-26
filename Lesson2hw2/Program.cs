4284542af6f22f42902c62720e8083a08c517253
sergey-yarchuk-4ib <sergey.yarchuk@4ib.com>
2021-09-26 21:10:30 +0300

    hw2

---
+++ Lesson2hw2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2hw2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			int a = 10; //=> 15
+			int b = 15; //=> 20
+			int c = 20; //=> 10
+
+			a = a ^ c;
+			c = a ^ c;
+			a = a ^ c;
+
+			a = a ^ b;
+			b = a ^ b;
+			a = a ^ b;
+
+
+			// awesome - C# has destructurization :D 
+			// best solution
+
+			// (a, b, c) = (b, c, a);
+
+			Console.WriteLine($"New a value: {a}");
+			Console.WriteLine($"New b value: {b}");
+			Console.WriteLine($"New c value: {c}");
+
+			Console.ReadKey();
+		}
+	}
+}
