ad361a32d9d05a4bc8e637f44ae851a9883e2003
sergey-yarchuk-4ib <sergey.yarchuk@4ib.com>
2021-09-26 15:36:21 +0300

    hw1

--- Lesson2hw1/Program.cs
+++ Lesson2hw1/Program.cs
@@ -10,6 +10,20 @@ namespace Lesson2hw1
 	{
 		static void Main(string[] args)
 		{
+			Console.WriteLine(CustomXOR(0, 0)); // false
+			Console.WriteLine(CustomXOR(1, 0)); // true
+			Console.WriteLine(CustomXOR(0, 1)); // true
+			Console.WriteLine(CustomXOR(1, 1)); // false
+
+			Console.ReadKey();
+		}
+
+		static bool CustomXOR(int a, int b)
+		{
+			bool x = Convert.ToBoolean(a);
+			bool y = Convert.ToBoolean(b);
+
+			return !x && y || x && !y;
 		}
 	}
 }
